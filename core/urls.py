34e2f6327139dda6458491dac8364b34a90e3c9b
ShailRT <shailesh18h@gmail.com>
2023-06-27 03:40:44 +0530

    reset-added

--- core/urls.py
+++ core/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path 
 from . import views
+from django.contrib.auth import views as auth_views
+
 
 urlpatterns = [
     path('', views.home, name="home"),
@@ -11,4 +13,9 @@ urlpatterns = [
     path('logout/', views.logout, name="logout"),
     path('signin/', views.signin, name="signin"),
     path('profile/<str:pk>/', views.profile, name="profile"),
+
+    path('reset_password/', auth_views.PasswordResetView.as_view(), name="password_reset"),
+    path('reset_password_sent/', auth_views.PasswordResetDoneView.as_view(), name="password_reset_done"),
+    path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(), name="password_reset_confirm"),
+    path('reset_password_complete/', auth_views.PasswordResetCompleteView.as_view(), name="password_reset_complete"),
 ]
